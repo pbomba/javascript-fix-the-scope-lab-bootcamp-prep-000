d5b1a1bed77d5b8b754df575a1757f3e3a7035db
Paul Bomba` <paul@paulbomba.com>
2018-05-05 21:30:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,14 +10,12 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  const two = 2;
+  return n + two;
 
-  // Feel free to move things around!
-  const two = 2
 }
 
-var funkyFunction = function() {
-  return function() {
+function funkyFunction() {
     return "FUNKY!"
   }
 }
